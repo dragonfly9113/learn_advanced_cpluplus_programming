49e9565168e68b2cbbef7ee985fc6d4b3a7afa52
Weilin Luo <weilin.luo@huawei.com>
2018-12-19 09:17:13 +0800

    add an exception

--- Basic Exceptions/basic.cpp
+++ Basic Exceptions/basic.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void mightGoWrong() {
+
+    bool error = true;
+
+    if(error) {
+        throw 8;
+    }
+}
+
 int main() 
 {
     cout << "Hello, World!" << endl;
+
+    mightGoWrong();
+
     return 0;
 }
