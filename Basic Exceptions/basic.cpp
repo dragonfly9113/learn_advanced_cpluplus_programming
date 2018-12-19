9bf262eda76d8e4395029b7cbf27553dd48d33d0
Weilin Luo <weilin.luo@huawei.com>
2018-12-19 08:52:43 +0800

    some change to basic.cpp

--- Basic Exceptions/basic.cpp
+++ Basic Exceptions/basic.cpp
@@ -1,7 +1,8 @@
-#include <stdio>
+#include <iostream>
+using namespace std;
 
-int main() {
-   
-   std::cout << "Hello World!" << std::endl;
-   return 0;
+int main() 
+{
+    cout << "Hello, World!" << endl;
+    return 0;
 }
