78aaccfad420277cfea257a84559b41d2f132735
Weilin Luo <weilin.luo@huawei.com>
2018-12-19 09:41:20 +0800

    finish basic exception example

--- Basic Exceptions/basic.cpp
+++ Basic Exceptions/basic.cpp
@@ -3,18 +3,40 @@ using namespace std;
 
 void mightGoWrong() {
 
-    bool error = true;
+    bool error1 = false;
+    bool error2 = true;
 
-    if(error) {
-        throw 8;
+    if(error1) {
+        throw "Something went wrong.";
     }
+
+    if(error2) {
+        throw string("Something else went wrong.");
+    }
+}
+
+void usesMightGoWrong() {
+    mightGoWrong();
 }
 
 int main() 
 {
     cout << "Hello, World!" << endl;
 
-    mightGoWrong();
+    try {
+        usesMightGoWrong();
+    }
+    catch(int e) {
+        cout << "Error code: " << e << endl;
+    }
+    catch(char const * e) {
+        cout << "Error message: " << e << endl;
+    }
+    catch(string &e) {
+        cout << "String error message: " << e << endl;
+    }
+    
+    cout << "Still running" << endl;
 
     return 0;
 }
