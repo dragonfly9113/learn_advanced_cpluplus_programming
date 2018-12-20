e21ca0348cac5542014e66cc786205df776e7464
William Luo <weilin.william.luo@gmail.com>
2018-12-19 23:24:09 -0800

    lecture 4: Standard Exceptions

---
+++ StandardExceptions/se.cpp
@@ -0,0 +1,27 @@
+// Name	: se.cpp
+
+#include <iostream>
+using namespace std;
+
+class CanGoWrong {
+public:
+    CanGoWrong() {
+	char *pMemory = new char[9999999999999999];
+	delete [] pMemory;
+    }
+};
+
+int main() {
+
+    try {
+        CanGoWrong wrong;
+    }
+    catch(bad_alloc &e) {
+	cout << "Caught exception: " << e.what() << endl;
+    }
+
+    cout << "Still running" << endl;
+
+    return 0;
+}
+
