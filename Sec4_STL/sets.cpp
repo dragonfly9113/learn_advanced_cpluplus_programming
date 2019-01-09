3061b1ed7998c1bf8d5a53b348e157318d53aebf
William <william_luo2004@yahoo.com>
2019-01-09 10:49:09 -0800

    lectore 20 finished.

--- Sec4_STL/sets.cpp
+++ Sec4_STL/sets.cpp
@@ -12,7 +12,7 @@ private:
 
 public:
     Person() : 
-	name(""), age(0) {}
+        name(""), age(0) {}
     
     Person(string name, int age) : 
 	name(name), age(age) {}
@@ -41,13 +41,33 @@ int main() {
     numbers.insert(50);
     numbers.insert(40);
     numbers.insert(10);
+    numbers.insert(10);
     numbers.insert(30);
 
     for (auto it = numbers.begin(); it != numbers.end(); it++) {
 	cout << *it << endl;
     }
 
+    // how to find a specific element?
+    auto it = numbers.find(15);
+
+    if (it != numbers.end()) {
+	cout << "Found: " << *it << endl;
+    } else {
+	cout << "Element not found!" << endl;
+    }
+
+    // a simpler way to check if an element is in a set
+    cout << endl;
+    if (numbers.count(20)) {
+	cout << "Found the number."  << endl;
+    } else {
+	cout << "Element not found!" << endl;
+    }
+
+
     // try to define a set of custom objects
+    cout << endl;
     set<Person> people;
 
     people.insert(Person("Mike", 50));
