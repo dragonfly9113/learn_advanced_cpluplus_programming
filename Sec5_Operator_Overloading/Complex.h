2643b96aaf0fbaf7b8e7ac5bfa78765881378188
William <william_luo2004@yahoo.com>
2019-01-15 18:24:19 -0800

    lecture 26 finished.

--- Sec5_Operator_Overloading/Complex.h
+++ Sec5_Operator_Overloading/Complex.h
@@ -3,6 +3,9 @@
 #ifndef COMPLEX_H_
 #define COMPLEX_H_
 
+#include <iostream>
+using namespace std;
+
 namespace caveofprogramming {
 
 class Complex {
@@ -14,12 +17,18 @@ public:
     Complex();
     Complex(double real, double imaginary);
     // copy constructor
-    Complex() 
+    Complex(const Complex &other);
+    // assignment operator overloading
+    const Complex &operator=(const Complex &other);
+
+    double getReal() const { return real; }
+    double getImaginary() const { return imaginary; }
 
 };
+
+ostream &operator<<(ostream &out, const Complex &c);
+
 
 } // namesapce caveofprogramming
 
 #endif // COMPLEX_H_
-
-
