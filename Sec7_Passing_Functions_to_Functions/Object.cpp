60674729ade2cfd6730399cd5fcdf5f256104f8d
William <william_luo2004@yahoo.com>
2019-03-08 18:45:15 -0800

    lecture 36: Object slicing and polymorphism

--- Sec7_Passing_Functions_to_Functions/Object.cpp
+++ Sec7_Passing_Functions_to_Functions/Object.cpp
@@ -4,32 +4,55 @@
 using namespace std;
 
 class Parent {
+private:
+    int one;
 
 public:
-    void print() {
-	cout << "parent" << endl;
+    Parent(): one(0) {
+
     }
 
+    Parent(const Parent &other): one(0) {
+
+        one = other.one;
+	cout << "copy parent" << endl;
+
+    }
+
+    virtual void print() {
+	cout << "parent: one = " << one << endl;
+    }
+
+    virtual ~Parent() {
+
+    }
 };
 
-
 class Child : public Parent {
+private:
+    int two;
 
 public:
-	void print() {
-	cout << "child" << endl;
+    Child(): two(1) {
 
     }
-    
+
+    void print() {
+        cout << "child: two = " << two << endl;
+    }
 };
 
 
 int main() {
 
     Child c1;
+    Parent &p1 = c1;
+    p1.print();
 
+    Parent p2 = Child();
+    p2.print();
 
-
+    return 0;
 }
 
 
