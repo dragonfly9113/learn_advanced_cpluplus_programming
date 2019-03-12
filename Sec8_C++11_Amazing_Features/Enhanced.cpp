359cf76da064929962937f24d32d1d7bed53d6e9
William <william_luo2004@yahoo.com>
2019-03-11 19:10:11 -0700

    lecture 41: Range-based Loops

---
+++ Sec8_C++11_Amazing_Features/Enhanced.cpp
@@ -0,0 +1,29 @@
+// Name :	Enhanced.cpp
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    auto texts = {"one", "two", "three"};
+
+    for (auto text : texts) {
+	cout << text << endl;
+    }
+
+    vector<int> numbers;
+    numbers.push_back(5);
+    numbers.push_back(7);
+    numbers.push_back(9);
+    numbers.push_back(11);
+
+    for (auto number : numbers) {
+	cout << number << endl;
+    }
+
+
+    return 0;
+}
+
