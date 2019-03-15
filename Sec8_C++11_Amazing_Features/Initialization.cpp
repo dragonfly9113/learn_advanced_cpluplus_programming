15104f7d172a298a26d0685bc170cfdafe855c51
William Luo <weilin.william.luo@gmail.com>
2019-03-14 22:21:49 -0700

    lecture 47: Initializer List

---
+++ Sec8_C++11_Amazing_Features/Initialization.cpp
@@ -0,0 +1,46 @@
+// Name :	Initialization.cpp
+
+
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+using namespace std;
+
+class Test {
+public:
+    Test(initializer_list<string> texts) {
+	for (auto value : texts) {
+	    cout << value << endl;
+	}
+    }
+
+    void print(initializer_list<string> texts) {
+	for (auto value : texts) {
+	    cout << value << endl;
+	}
+    }
+    
+    void printv(vector<string> texts) {
+	for (auto value : texts) {
+	    cout << value << endl;
+	}
+    }
+};
+
+
+int main() {
+
+    vector<int> numbers{1, 3, 4, 6};
+    cout << numbers[2] << endl;
+
+    Test test{"apple", "orange", "banana"};
+
+    test.print({"one", "two", "three", "four"});
+
+    test.printv(vector<string>{"A", "B"});
+
+    return 0;
+}
+
+
+
