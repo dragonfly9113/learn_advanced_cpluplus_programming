ed6380c01ce6cee21bff068f2aa984c97a61277a
William Luo <weilin.william.luo@gmail.com>
2019-04-03 18:24:25 -0700

    Lecture 68: Unique Pointers

---
+++ Sec8_C++11_Amazing_Features/Unique.cpp
@@ -0,0 +1,45 @@
+// Name :	Unique.cpp
+
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class Test {
+public:
+    Test() {
+	cout << "created" << endl;
+    }
+
+    void greet() {
+	cout << "Hello" << endl;
+    }
+
+    ~Test() {
+	cout << "destroyed" << endl;
+    }
+};
+
+class Temp {
+private:
+    unique_ptr<Test> pTest;
+
+public:
+    Temp() : pTest(new Test) {}
+};
+
+
+int main() {
+
+    Temp temp;
+
+    //unique_ptr<Test[]> pTest(new Test[2]);
+
+    //pTest[1].greet();
+
+    cout << "Finished" << endl;
+
+    return 0;
+}
+
+
