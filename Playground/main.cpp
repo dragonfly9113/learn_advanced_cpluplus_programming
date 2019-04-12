424656a5c3a13bb27e2a6cde3935843c6c27c844
William <william_luo2004@yahoo.com>
2019-04-12 14:37:25 -0700

    Playground

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -3,11 +3,29 @@
 #include <iostream>
 using namespace std;
 
+double vals[] = {10.1, 12.6, 33.1, 24.1, 50.0};
+
+double& setValues(int i) {
+    double v[] = {11.0, 22.0, 33.0, 44.0, 55.0};
+
+    return v[i];
+    //return vals[i];
+}
 
 int main() {
 
-    cout << "Hello world!" << endl;
+    for (int i = 0; i < 5; i++) {
+	cout << vals[i] << " ";
+    }
+    cout << endl;
 
+    setValues(1) = 20.23;
+    setValues(3) = 70.8;
+
+    for (int i = 0; i < 5; i++) {
+	cout << vals[i] << " ";
+    }
+    cout << endl;
 
 
     return 0;
