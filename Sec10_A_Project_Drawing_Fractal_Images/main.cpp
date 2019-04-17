86fc7d303a631c89c6da9bee18abffde8c8914f9
William Luo <weilin.william.luo@gmail.com>
2019-04-16 22:35:58 -0700

    Lecture 80: Scaling

--- Sec10_A_Project_Drawing_Fractal_Images/main.cpp
+++ Sec10_A_Project_Drawing_Fractal_Images/main.cpp
@@ -13,13 +13,24 @@ int main() {
 
     Bitmap bitmap(WIDTH, HEIGHT);
 
+    double min = 999999;
+    double max = -999999;
+
     for (int y = 0; y < HEIGHT; y++)
     {
 	for (int x = 0; x < WIDTH; x++)
 	{
-	    bitmap.setPixel(x, y, 255, 0, 0);
+	    double xFractal = (x - WIDTH/2) * 2.0 / WIDTH;
+	    double yFractal = (y - HEIGHT/2) * 2.0 / HEIGHT;
+
+	    if (xFractal < min) min = xFractal;
+	    if (xFractal > max) max = xFractal;
+
 	}
     }
+
+    cout << min << ", " << max << endl;
+
 
     if (bitmap.write("test.bmp")) {
 	cout << "Bitmap writing OK" << endl;
