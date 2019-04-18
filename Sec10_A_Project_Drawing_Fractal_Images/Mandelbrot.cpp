5fcfbfa45c2936212fda74f5fceaeed5fb0049e7
William Luo <weilin.william.luo@gmail.com>
2019-04-17 22:45:57 -0700

    Lecutre 81: A Mandelbrot Class

---
+++ Sec10_A_Project_Drawing_Fractal_Images/Mandelbrot.cpp
@@ -0,0 +1,22 @@
+// Name :	Mandelbrot.cpp
+
+#include "Mandelbrot.h"
+
+namespace caveofprogramming {
+
+Mandelbrot::Mandelbrot() {
+
+
+}
+
+Mandelbrot::~Mandelbrot() {
+
+
+}
+
+int Mandelbrot::getIterations(double x, double y) {
+
+    return 0;
+}
+
+}  // namespace caveofprogramming
