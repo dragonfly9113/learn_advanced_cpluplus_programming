d5b82486ce077ea3118d8952b696ffc4c9c5f75b
William <william_luo2004@yahoo.com>
2019-04-19 12:33:04 -0700

    Lecture 84: The Color Histogram

--- Sec10_A_Project_Drawing_Fractal_Images/main.cpp
+++ Sec10_A_Project_Drawing_Fractal_Images/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cstdint>
+#include <memory>
 #include "Bitmap.h"
 #include "Mandelbrot.h"
 
@@ -18,21 +19,27 @@ int main() {
     double min = 999999;
     double max = -999999;
 
+    unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS + 1]{});
+
     for (int y = 0; y < HEIGHT; y++)
     {
 	for (int x = 0; x < WIDTH; x++)
 	{
-	    double xFractal = (x - WIDTH/2) * 2.0 / WIDTH;
+	    double xFractal = (x - WIDTH/2 - 200) * 2.0 / HEIGHT; 
 	    double yFractal = (y - HEIGHT/2) * 2.0 / HEIGHT;
 
 	    int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
-	    uint8_t red = (uint8_t)(256 * (double)iterations/Mandelbrot::MAX_ITERATIONS);
+	    histogram[iterations]++;
 
-	    bitmap.setPixel(x, y, red, red, red);
+	    uint8_t color = (uint8_t)(256 * (double)iterations/Mandelbrot::MAX_ITERATIONS);
 
-	    if (red < min) min = red;
-	    if (red > max) max = red;
+	    color = color * color * color;
+
+	    bitmap.setPixel(x, y, 0, color, 0);
+
+	    if (color < min) min = color;
+	    if (color > max) max = color;
 
 	}
     }
